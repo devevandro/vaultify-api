f53240a0d8c6ec8d6f6ba71f0ae1509cc8ed2716
devevandro <dev.evandro@gmail.com>
2024-11-20 21:15:10 -0300

    feat: create middleware to api key

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { AppController } from './main/http/controllers/AppController';
 import { UserController } from './main/http/controllers/UserController';
+import { ApiKeyMiddleware } from './main/http/middleware/ApiKeyMiddleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController, UserController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware).forRoutes('/users');
+  }
+}
