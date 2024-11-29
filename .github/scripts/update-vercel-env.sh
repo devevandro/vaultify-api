2d8ee7b68c6ae76b04f19a3ae04e40e55abfb29e
devevandro <dev.evandro@gmail.com>
2024-11-28 22:48:21 -0300

    feat: remove vars

--- .github/scripts/update-vercel-env.sh
+++ .github/scripts/update-vercel-env.sh
@@ -11,8 +11,22 @@ TOKEN=$3
 update_env_vars() {
   echo "Updating environment variables for environment: $ENVIRONMENT"
 
+  # Variáveis que queremos excluir
+  exclude_vars=(
+    "VERCEL_DOMAIN_PROJECT_PROD"
+    "VERCEL_DOMAIN_PROJECT_STAGE"
+    "VERCEL_ORG_ID"
+    "VERCEL_PROJECT_ID"
+    "VERCEL_TOKEN"
+    "github_token"
+  )
+
+  # Construir o filtro jq para excluir as variáveis listadas
+  jq_filter=$(printf 'select(.key != "%s") | ' "${exclude_vars[@]}")
+  jq_filter="${jq_filter::-3}"
+
   # Itera sobre cada par chave-valor no JSON
-  echo "$SECRETS_JSON" | jq -r 'to_entries[] | "\(.key)=\(.value)"' | while IFS= read -r VAR; do
+  echo "$SECRETS_JSON" | jq -r "to_entries | map($jq_filter) | map(\"\(.key)=\(.value)\") | join(\" \")" | while IFS= read -r VAR; do
     # Extrai chave e valor
     VARIABLE_NAME=$(echo "$VAR" | cut -d '=' -f 1)
     VALUE=$(echo "$VAR" | cut -d '=' -f 2-)
